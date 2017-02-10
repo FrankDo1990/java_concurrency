8c33937a0343479270b561dbe8d95024b353e6fb
frank <dufeng1122@yeah.net>
2017-02-10 17:52:26 +0800

    merge

--- src/test/java/TestClass.java
+++ src/test/java/TestClass.java
@@ -1,19 +1,21 @@
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.TypeReference;
 import com.frank.concurrency.utils.Utils;
-import org.junit.BeforeClass;
 import org.junit.Test;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Frank on 2017/1/28.
  */
+
 public class TestClass {
-
-    @BeforeClass
-    public static void init(){
-
-    }
 
     @Test
     public  void testFormat(){
-        Utils.formatPrintLn("hello ? {} no {} worlf {}", true, 2, 3);
+        String json = "[101,102,105,108,120,121,148,100001,100002,100003,100004,100005,50000032]";
+        List<Integer> res = JSON.parseArray(json, Integer.class);
+        System.out.println(res);
     }
 }
